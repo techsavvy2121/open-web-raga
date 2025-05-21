9645eda24919f3ddd014cdea83f48a989b3c79e5
Suyash Vaishnava <suyash.v@novatiosolutions.com>
2025-05-21 09:37:06 -0500

    Updated patch.sh to fix Dockerfile for legacy builds

--- patch.sh
+++ patch.sh
@@ -6,6 +6,8 @@ rm -rf open-webui
 # Step 2: Clone official Open WebUI
 git clone https://github.com/open-webui/open-webui.git
 
+sed -i 's/FROM --platform=\$BUILDPLATFORM node/FROM node/' open-webui/Dockerfile
+
 # Step 3: Build your custom Docker image
 cd open-webui
 docker build -t open-web-raga .
